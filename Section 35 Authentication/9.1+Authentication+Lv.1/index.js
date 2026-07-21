3167d33b1ce721ddd5432df57fc5872d160ec878
George Bahij <georgesoukariah@gmail.com>
2026-07-21 13:03:02 +0300

    Section 35 Authentication

---
+++ Section 35 Authentication/9.1+Authentication+Lv.1/index.js
@@ -0,0 +1,89 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import bcrypt from "bcryptjs";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "secrets",
+  password: "",
+  port: 5432,
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  try {
+    const result = await db.query(
+      `
+      INSERT INTO users
+      VALUES ($1, $2)
+      `,
+      [email, password],
+    );
+    console.log(result.rows);
+    res.render("secrets.ejs");
+  } catch (err) {
+    if (err.code === "23505") {
+      console.log("Email already exists");
+    } else {
+      console.log(err);
+    }
+    res.redirect("/");
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  const result = await db.query(
+    `
+    SELECT *
+    FROM users
+    WHERE email = $1
+    AND password = $2
+    `,
+    [email, password],
+  );
+  if (result.rowCount <= 0) {
+    res.send("invalid email or password");
+  } else {
+    console.log(result.rows);
+    res.render("secrets.ejs");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+process.on("SIGTERM", () => {
+  db.end();
+  console.log("server terminated");
+});
+
+process.on("SIGINT", () => {
+  db.end();
+  console.log("server terminated");
+});
